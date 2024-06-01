a93cb74a6acfe8ee460bac979c753f3ef10dca0b
Mochammad Alie <mochammad.aliesf@gmail.com>
2024-06-01 17:24:53 +0700

    Update Add to Cart Module

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# TAK5-Final-Exercise
+# Final Exercise - QA Automation Engineer
+
+--------
+
+This final exercise was completed by Team 2, consisting of the following members:
+1. Tri Putra Saragih
+2. Fadila Maulidia
+3. Mochammad Alie Shodiq F
+
+## Test Scripts Coverage
+
+The test scripts cover the following modules:
+1. **Module Registration** - Tri Putra Saragih
+2. **Module Edit Account** - Fadila Maulidia
+3. **Module Add to Cart** - Mochammad Alie Shodiq F
+4. **Module Checkout** - Mochammad Alie Shodiq F
+
+You can refer to the test cases in the [spreadsheet](https://docs.google.com/spreadsheets/d/1J5lHo_W8_MMDhMNZr6wR5BxWuD3wj7k3arXpZP1faSE/edit#gid=0).
--- cypress/e2e/4-Proceed-To-Checkout/AddToCart.cy.js
+++ cypress/e2e/4-Proceed-To-Checkout/AddToCart.cy.js
@@ -21,7 +21,8 @@ describe('Test Script for Proceed to Checkout Module', () => {
         product.click()
 
         // Add the item to shopping cart
-        cy.get('#product-addtocart-button').should('be.visible').click()
+        const btnAddToCart = cy.get('#product-addtocart-button').should('be.visible')
+        btnAddToCart.click()
 
         // Validation
         const successMsg = cy.contains('You added Fusion Backpack to your shopping cart.')
@@ -43,9 +44,60 @@ describe('Test Script for Proceed to Checkout Module', () => {
 
         const btnRemove = cy.get('.product > .secondary > .action').click()
 
-        const btnConfirm = cy.contains('OK').should('be visible').click ()
+        const btnConfirm = cy.contains('OK').click ()
 
         cy.get('.block-minicart').should('be.visible')
         cy.contains('You have no items in your shopping cart.').should('be.visible')
+    })
+
+    // Test Script TC003
+    it('Update item qty below than 0', () => {
+
+        cy.get('#email').type('budihartono12@gmail.com')
+        cy.get('#pass').type('Budi123@')
+        
+        // Define login button
+        const btnLogin = cy.get('#send2')
+        btnLogin.click()
+
+        cy.visit('https://magento.softwaretestingboard.com/gear.html')
+        const product = cy.contains('Fusion Backpack')
+        product.click()
+
+        cy.get('#qty').then(($input) => {
+            $input.attr('value', '-5')
+            $input.trigger('input')
+        })
+
+        // Add the item to shopping cart
+        const btnAddToCart = cy.get('#product-addtocart-button').should('be.visible')
+        btnAddToCart.click()
+
+        // Validation
+        cy.contains('Please enter a quantity greater than 0.').should('be.visible')
+    })
+
+    // Test script TC004
+    it.only('Proceed to checkout with 1 item', () => {
+
+        cy.get('#email').type('budihartono12@gmail.com')
+        cy.get('#pass').type('Budi123@')
+        
+        // Define login button
+        const btnLogin = cy.get('#send2')
+        btnLogin.click()
+
+        cy.contains('Welcome, Budi Hartono!').should('be.visible')
+        
+        // Visit the product item
+        const btnCart = cy.get('.showcart')
+        btnCart.click()
+
+        // Proceed to Checkout
+        cy.get('#top-cart-btn-checkout').should('be.visible').and('be.enabled').click()
+
+        // Validation
+        cy.url().should('include', '/checkout/#shipping')
+        cy.contains('Shipping Address').should('be.visible')
     })
 })
