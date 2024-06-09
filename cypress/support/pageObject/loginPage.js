77724c3ac4024a52b8b4a833d49acd507e0e99ca
Angeli <26angelsilaban@gmail.com>
2024-06-09 18:03:46 +0700

    Update POM loginPage

--- cypress/support/pageObject/loginPage.js
+++ cypress/support/pageObject/loginPage.js
@@ -2,6 +2,24 @@ class loginPage {
     emailField = "#email"
     passField = "#pass"
     loginButton = "#send2"
+    msgError = '.message-error > div'
+    emailError = '#email-error'
+
+    inputEmail(email) {
+        cy.get(this.emailField).type(email)
+    }
+    inputPass(pass) {
+        cy.get(this.passField).type(pass)
+    }
+    buttonLogin(){
+        cy.get(this.loginButton).click()
+    }
+    errorMsg(error,message){
+        cy.get(error).should('contain.text',message)
+    }
+    // errorMail(message){
+    //     cy.get(this.emailerror).should('contain.text',message)
+    // }
 }
 
 export default new loginPage()
